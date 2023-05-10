cbbfae1dd5d7bdb02177e6b30f50959235d8f63f
Wyatt Strother <wstrother@users.noreply.github.com>
2023-05-10 13:40:03 -0400

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<h1>N OVER 2</h1>
+
+<div>
+    <h2>Player 1</h2>
+    <p>choosing...</p>
+</div>
+
+<div>
+    <h2>Player 2</h2>
+    <p>choosing...</p>
+</div>
+
+<div>
+    <h2>Pick a Number</h2>
+</div>
+

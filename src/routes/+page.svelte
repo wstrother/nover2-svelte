1136a34662d48d0137ee9555699b29dbc359d359
Wyatt Strother <wstrother@users.noreply.github.com>
2023-05-11 13:39:04 -0400

    testing auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,46 @@
+<script>
+    import { supabase } from "../lib/supabaseClient";
+    let user = '';
+    let email;
+    let password;
+
+    const login = async () => {
+        const { data, error } = await supabase.auth.signInWithPassword({email, password});
+
+        // if ( error ) {
+        //     console.log('error')
+        // } else {
+        //     console.log(data)
+        // }
+    }
+
+    const logout = async () => {
+        const { error } = await supabase.auth.signOut()
+
+        // if ( error ) {
+        //     console.log(error)
+        // }
+    }
+
+    supabase.auth.onAuthStateChange((event, session) => {
+        console.log(event, session)
+        if (session) user = session.user.email
+        else user = ''
+    })
+
+</script>
 <h1>N OVER 2</h1>
+{#if user }
+    <p>Welcome, {user}</p>
+{/if}
 
-<div>
-    <h2>Player 1</h2>
-    <p>choosing...</p>
-</div>
 
-<div>
-    <h2>Player 2</h2>
-    <p>choosing...</p>
-</div>
+<form on:submit|preventDefault={login}>
+    <input type="email" bind:value={email} />
+    <input type="password" bind:value={password} />
+    <input type="submit" />
+</form>
 
-<div>
-    <h2>Pick a Number</h2>
-</div>
+<p></p>
 
+<button on:click={logout}>Log Out</button>

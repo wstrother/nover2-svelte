b60342f16b87c7ff0c3bcd71783bbf2beb4b5c4c
Wyatt Strother <wstrother@users.noreply.github.com>
2023-05-11 14:22:18 -0400

    login component and auth functions

--- src/lib/components/login.svelte
+++ src/lib/components/login.svelte
@@ -1,34 +1,13 @@
 <script>
-    import { supabase } from "$lib/supabaseClient";
+    import { user } from "$lib/stores/auth.js";
 
     let email = "";
     let password = "";
 
-    const login = async () => {
-        const { data, error } = await supabase.auth.signInWithPassword({email, password});
-
-        // if ( error ) {
-        //     console.log('error')
-        // } else {
-        //     console.log(data)
-        // }
-    }
-
-    const logout = async () => {
-        const { error } = await supabase.auth.signOut()
-
-        // if ( error ) {
-        //     console.log(error)
-        // }
-    }
 </script>
 
-<form on:submit|preventDefault={login}>
+<form on:submit|preventDefault={() => user.login(email, password)}>
     <input type="email" bind:value={email} />
     <input type="password" bind:value={password} />
     <input type="submit" />
 </form>
-
-<p></p>
-
-<button on:click={logout}>Log Out</button>
--- src/lib/stores/auth.js
+++ src/lib/stores/auth.js
@@ -9,3 +9,21 @@ supabase.auth.onAuthStateChange((event, session) => {
     if (session) user.set(session.user)
     else user.set(null);
 })
+
+user.login = async (email, password) => {
+    const { data, error } = await supabase.auth.signInWithPassword({email, password});
+
+    // if ( error ) {
+    //     console.log('error')
+    // } else {
+    //     console.log(data)
+    // }
+}
+
+user.logout = async () => {
+    const { error } = await supabase.auth.signOut()
+
+    // if ( error ) {
+    //     console.log(error)
+    // }
+}

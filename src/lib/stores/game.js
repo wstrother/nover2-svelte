6e2f10d64ded3dbcb2ca268a22a6ce27fffc35c5
Wyatt Strother <wstrother@users.noreply.github.com>
2023-05-12 22:49:02 -0400

    added turns to game.js store

--- src/lib/stores/game.js
+++ src/lib/stores/game.js
@@ -2,6 +2,19 @@ import { supabase } from "$lib/supabaseClient";
 import { writable } from "svelte/store";
 
 
+class Turn {
+    constructor(id, host_move, opponent_move) {
+        this.id = id;
+        this.host_move = host_move;
+        this.opponent_move = opponent_move;
+    }
+
+    get_from_data(data) {
+        return data.map(turn => new Turn(turn.id, turn.host_move, turn.opponent_move));
+    }
+}
+
+
 class Game {
     constructor() {
         this.room_id = null;
@@ -24,6 +37,22 @@ class Game {
     reset_data() {
         this.update_data(new Game());
     }
+
+    set_turns(data) {
+        this.turns = data.map(turn => new Turn(turn.id, turn.host_move, turn.opponent_move));
+    }
+
+    get_current_turn() {
+        if (this.turns.length) return turns[this.turns.length-1];
+
+        return {};
+    }
+
+    update_turn(data) {
+        const turn = this.turns.filter(t => t.id == data.id)[0];
+        turn.host_move = data.host_move;
+        turn.opponent_move = data.opponent_move;
+    }
 }
 
 const current_game = new Game();
@@ -32,19 +61,37 @@ export const game = new writable(current_game);
 
 game.load_data = async () => {
     if (current_game.room_id) {
-        const { data, error} = await supabase
+        const {data, error} = await supabase
             .from('rooms')
             .select('*')
             .eq('id', current_game.room_id);
         
-        if ( error ) console.log(error);
-        if ( data && data[0]) {
+        if (error) console.log(error);
+        if (data && data[0]) {
+
             current_game.viewable = true;
             current_game.update_data(data[0]);
+            game.load_turns();
+
         } else {
             current_game.reset_data();
         }
     }
+    game.set(current_game);
+}
+
+game.load_turns = async () => {
+    const {data, error} = await supabase
+        .from('turns')
+        .select('*')
+        .eq('room_id', current_game.room_id)
+        .order('created_at');
+
+        if (error) console.log(error);
+        if (data) {
+            current_game.set_turns(data);
+        }
+    
     game.set(current_game);
 }
 
@@ -55,11 +102,21 @@ supabase.auth.onAuthStateChange(() => {
 game.set_room_id = (room_id) => {
     current_game.room_id = room_id;
 
-    supabase.channel('any')
-        .on('postgres_changes', { event: 'UPDATE', schema: 'public', table: 'rooms', filter: `id=eq.${room_id}`}, payload => {
+    supabase
+        .channel('any')
+        .on('postgres_changes', {event: 'UPDATE', schema: 'public', table: 'rooms', filter: `id=eq.${room_id}`}, payload => {
             if (payload.new) {
-                console.log(payload.new);
                 current_game.update_data(payload.new);
+                game.set(current_game);
+            }
+        })
+        .subscribe();
+    
+    supabase
+        .channel('any')
+        .on('postgres_changes', {event: '*', schema: 'public', table: 'turns', filter: `room_id=eq.${room_id}`}, payload => {
+            if (payload.new) {
+                current_game.update_turn(payload.new);
                 game.set(current_game);
             }
         })

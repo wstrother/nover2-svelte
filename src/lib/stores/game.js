4b9c9645ceb8ce697ef30d3c7b43729cee584f37
Wyatt Strother <wstrother@users.noreply.github.com>
2023-05-14 23:07:28 -0400

    no more turn class

--- src/lib/stores/game.js
+++ src/lib/stores/game.js
@@ -2,19 +2,6 @@ import { supabase } from "$lib/supabaseClient";
 import { writable } from "svelte/store";
 
 
-class Turn {
-    constructor(id, host_move, opponent_move) {
-        this.id = id;
-        this.host_move = host_move;
-        this.opponent_move = opponent_move;
-    }
-
-    get_from_data(data) {
-        return data.map(turn => new Turn(turn.id, turn.host_move, turn.opponent_move));
-    }
-}
-
-
 class Game {
     constructor() {
         this.viewable = null;
@@ -47,22 +34,6 @@ class Game {
 
     reset_data() {
         this.update_data(new Game());
-    }
-
-    // set_turns(data) {
-    //     this.turns = data.map(turn => new Turn(turn.id, turn.host_move, turn.opponent_move));
-    // }
-
-    // get_current_turn() {
-    //     if (this.turns.length) return turns[this.turns.length-1];
-
-    //     return {};
-    // }
-
-    update_turn(data) {
-        const turn = this.turns.filter(t => t.id == data.id)[0];
-        turn.host_move = data.host_move;
-        turn.opponent_move = data.opponent_move;
     }
 }
 

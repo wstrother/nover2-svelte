ae179028f80484248325fc34b868fc6dc8131bbc
Wyatt Strother <wstrother@users.noreply.github.com>
2023-05-18 16:18:40 -0400

    null coalesce scores

--- src/lib/components/room/score.svelte
+++ src/lib/components/room/score.svelte
@@ -5,5 +5,5 @@
 
 <div class='flex flex-col items-center my-4 self-center h3 variant-soft-primary rounded py-1 px-3 mx-auto w-1/4 min-w-max'>
     <span class='player_name'>{player_name}</span>
-    <span class='player_score'>{player_score}</span>
+    <span class='player_score'>{player_score ?? 0}</span>
 </div>

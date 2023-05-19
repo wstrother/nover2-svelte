9aa309ea5cccc934eb69a15c9d48afed62ca0367
Wyatt Strother <wstrother@users.noreply.github.com>
2023-05-18 22:09:02 -0400

    styled wrapup

--- src/lib/components/room/wrapup.svelte
+++ src/lib/components/room/wrapup.svelte
@@ -2,8 +2,10 @@
     export let game;
 </script>
 
-<div>
+<div class="self-center flex flex-col items-center h1">
     <h1>{`${game.winner_name} takes it over ${game.loser_name}!`}</h1>
-    <div>Final Score</div>
-    <div>{`${game.winner_score} - ${game.loser_score}`}</div>
+    <div class="h2 flex flex-col items-center variant-ghost-secondary rounded p-1 my-5 w-1/2">
+        Final Score
+        <div class="h1">{`${game.winner_score} - ${game.loser_score}`}</div>
+    </div>
 </div>
